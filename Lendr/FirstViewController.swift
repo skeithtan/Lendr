ff643a9bd119e561599958cc03673a2334a041a3
Keith Tan <skeithtan@gmail.com>
2017-10-12 07:46:11 +0800

    Initial Commit

---
+++ Lendr/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Lendr
+//
+//  Created by Keith Tan on 12/10/2017.
+//  Copyright © 2017 Axis. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
