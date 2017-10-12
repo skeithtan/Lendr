b7a34d6663fb16b230dc22a7f56848bef08b37e4
Keith Tan <skeithtan@gmail.com>
2017-10-12 08:34:12 +0800

    Added Realm, Cocoapods, gitignore

---
+++ Pods/Target Support Files/Pods-Lendr/Pods-Lendr-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Lendr : NSObject
+@end
+@implementation PodsDummy_Pods_Lendr
+@end
---
+++ Pods/Target Support Files/Pods-LendrTests/Pods-LendrTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LendrTests : NSObject
+@end
+@implementation PodsDummy_Pods_LendrTests
+@end
